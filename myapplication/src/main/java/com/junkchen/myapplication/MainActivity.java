ccdac02c2c22036c3018e54a5a6ef9ae0cb29da6
JunkChen <junkchen@vip.qq.com>
2015-11-12 14:17:54 +0800

    update MainActivity.java file

--- myapplication/src/main/java/com/junkchen/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/junkchen/myapplication/MainActivity.java
@@ -2,8 +2,12 @@ package com.junkchen.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+	private TextView textView;
+	private Button button;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
